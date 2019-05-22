9441473cf4fd25d679c70872e6d0e2e9b3400bcd
Elias177 <eliassantiago177@gmail.com>
2019-05-22 14:23:50 -0400

    Parte 2. Mover una esfera
    Parte 2 de las entregas de curso en linea

---
+++ Unity Tutoriales/Ready2Roll/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject chungus;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - chungus.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = chungus.transform.position + offset;
+    }
+}
---
+++ Unity Tutoriales/Ready2Roll/Assets/Scripts/ChungusController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChungusController : MonoBehaviour
+{
+
+    
+
+    public float speed;
+    public Text scoreText;
+    public Text winText;
+    public AudioSource bite;
+
+    private Rigidbody rb;
+    private int scoreCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        scoreCount = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(Horizontal, 0.0f, Vertical);
+
+        rb.AddForce(movement * speed); 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Carrot"))
+        {
+            other.gameObject.SetActive(false);
+            scoreCount = scoreCount + 1;
+            SetCountText();
+            bite.Play();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Carrots: " + scoreCount.ToString();
+        if (scoreCount >= 11)
+        {
+            winText.text = "Exito!";
+        }
+    }
+}
