f3e00490a56399a0dda712378d7bff8e46fc48cc
Elias Santiago <eliassantiago177@gmail.com>
2019-06-06 02:16:08 -0400

    Integracion con el web service.
    Falta arreglar el serializado del json

--- PirataRPG/PirataRPG/Assets/Scripts/GlobalScript.cs
+++ PirataRPG/PirataRPG/Assets/Scripts/GlobalScript.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GlobalScript : MonoBehaviour
 {
     public int LeftScore;
     public int RightScore;
+
+
 
     public TextMesh LeftScoreText;
     public TextMesh RightScoreText;
@@ -22,7 +26,16 @@ public class GlobalScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(2))
+        {
+            Score s = new Score();
+            s.name = "Unity";
+            s.score = 100f;
+            StartCoroutine(SendRequest("http://localhost:2468/api/Score/GetScores"));
+            Debug.Log("********************");
+            StartCoroutine(PostRequest("http://localhost:2468/api/Score/UploadScore?data=", s));
+
+        }
     }
 
     public void IncrementScore(bool IsLeftPlayer, bool oneplayer)
@@ -48,6 +61,46 @@ public class GlobalScript : MonoBehaviour
             Success.Play();
 
         }
+
+    }
+
+
+    IEnumerator SendRequest(string url)
+    {
+
+        UnityWebRequest webRequest = UnityWebRequest.Get(url);
+
+        yield return webRequest.SendWebRequest();
+
+        Debug.Log(webRequest.downloadHandler.text);
+
+    }
+
+    IEnumerator PostRequest(string url, Score score)
+    {
+        WWWForm form = new WWWForm();
+
+
+        string data = JsonUtility.ToJson(score,true);
+        Debug.Log(data);
+        
+        form.AddField("data",data);
+
+        UnityWebRequest webRequest = UnityWebRequest.Post(url, form);
+
+        webRequest.method = "POST";
+       
+        yield return webRequest.SendWebRequest();
+
+        Debug.Log(webRequest.downloadHandler.text);
+
+    }
+
+    [Serializable]
+    public class Score
+    {
+        public string name { get; set; }
+        public float score { get; set; }
 
     }
 }
